987692eb12608e43a55ee20819e55bf3122cbe60
unknown <karandasheva@biocad.ru>
2023-07-17 21:08:02 +0300

    исправлена реализация 4-ой задачи

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,14 +1,5 @@
 function showSalary(users, age) {
-  let result = "";
-  users.filter(user =>user.age <= age)
+  return users.filter(user =>user.age <= age)
   .map(user => user.name +", "+ user.balance)
-  .forEach((element,index,arr)=>{
-      if(index == arr.length-1){
-        result = result + element;
-      }else {
-          result = result + element + '\n';
-      }
-  });
-  
-  return result;
+  .join('\n');
 }
